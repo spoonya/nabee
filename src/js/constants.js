74d260445142fa6ed4f76a01cdb070b0760d11f5
spoonya <nomad8t@gmail.com>
2022-04-14 13:23:09 +0300

    init: project setup

---
+++ src/js/constants.js
@@ -0,0 +1,13 @@
+export const CLASSES = {
+  active: 'active',
+  loading: 'loading',
+  scrollHidden: 'scroll-hidden',
+  modal: 'modal',
+  scroll: 'scroll',
+  notEmpty: 'not-empty'
+};
+
+export const DOM = {
+  body: document.querySelector('body'),
+  overlay: document.querySelector('#overlay')
+};
